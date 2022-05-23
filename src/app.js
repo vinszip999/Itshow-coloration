362aedf7ad7db410c90c3f4bff47fc2e241ff257
kim mina <kimmina9070@gmail.com>
2022-05-23 10:19:25 +0900

    delete

--- src/app.js
+++ src/app.js
@@ -5,20 +5,20 @@ app.listen(8080, function(){
   console.log('8080');
 })
 app.get('/',function(req,res){
-  res.sendFile(__dirname +'./index.html');
+  res.sendFile(__dirname + './public/index.html');
 })
 app.get('/board',function(req,res){
-  res.sendFile(__dirname+'./board.html');
+  res.sendFile(__dirname+'./public/board.html');
 })
 app.get('/calender',function(req,res){
-  res.sendFile(__dirname+'./calender.html');
+  res.sendFile(__dirname+'./public/calender.html');
 })
 app.get('/detail',function(req,res){
-  res.sendFile(__dirname+'./detail.html');
+  res.sendFile(__dirname+'./public/detail.html');
 })
 app.get('/signup',function(req,res){
-  res.sendFile(__dirname+'./signup.html');
+  res.sendFile(__dirname+'./public/signup.html');
 })
 app.get('/terms',function(req,res){
-  res.sendFile(__dirname+'./terms.html');
+  res.sendFile(__dirname+'./public/terms.html');
 })

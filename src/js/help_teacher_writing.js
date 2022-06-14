3a56e94ee17ede420bd20e5dfe0f7413e63955a0
vinszip999 <w2011@e-mirim.hs.kr>
2022-06-15 00:03:18 +0900

    [0615] 교사도움신청 형식 완성

---
+++ src/js/help_teacher_writing.js
@@ -0,0 +1,16 @@
+// document.getElementById("btn").addEventListener('click',event);
+
+function event(){
+    window.location.href='help_teacher_student.html'
+    alert('신청이 완료되었습니다.');
+}
+
+// document.getElementById('btn').onclick = function(){
+//     window.location.href='help_teacher_student.html'
+    
+//     }
+
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click", function() {
+// 	//Do something here
+// }, false);

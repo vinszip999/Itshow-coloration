5390e4ad7795c36c1d14aba27523206475e4629c
kimmina <kimmina9070@gmail.com>
2022-06-16 22:19:26 +0900

    new

--- src/mysqlFunction.js
+++ src/mysqlFunction.js
@@ -20,7 +20,7 @@ if(connection.connect()){
 //   console.log('User info is: ', rows);
 // });
 
-function login(userid, userpw){
+function Login(userid, userpw){
     connection.query('SELECT * from user where user_id='+userid+'and user_pw = '+userpw, (error, rows, fields) => {
         if (error) throw error;
         console.log('User info is: ', rows);
@@ -30,5 +30,27 @@ function login(userid, userpw){
 }
 
 function Signup(user_id, user_pw, user_name, user_email, user_phone, user_type, user_proimg){
-  
+  connection.query('INSERT INTO USERS VALUES('+user_id+', '+user_pw+', '+user_name+', '+user_email+', '+user_phone+', '+user_type+', '+user_proimg+')', (error, rows, fields) => {
+    if (error) throw error;
+  });
+}
+
+function Select_Post(){
+  connection.query('SELECT * from post', (error, rows, fields) => {
+    if (error) throw error;
+  });
+  return rows;
+}
+
+function Select_Like_Post(user_id){
+  connection.query('SELECT * from like_post where user_id ='+user_id, (error, rows, fields) => {
+    if (error) throw error;
+  });
+  return rows;
+}
+
+function Upload_post(post_title, post_map, post_imgid, post_text, post_user){
+  connection.query('INSERT INTO post VALUES('+post_title+', '+post_map+', '+post_imgid+', '+post_text+', '+post_user+')', (error, rows, fields) => {
+    if (error) throw error;
+  });
 }
